c35237269b1d77b69fe30dd040a39ce81b812f9d
xiaoyu <xiaoyu@sulg.top>
2022-12-14 23:25:57 +0800

    完成按钮组件

--- src/component/plumeUi/Button/index.tsx
+++ src/component/plumeUi/Button/index.tsx
@@ -1,10 +1,19 @@
 import { preName } from "@plumeUi/var";
+import { ParentProps } from "solid-js";
 import "./index.less";
-const Button = (props) => {
-  const { children, onClick, type = "primary" } = props;
+
+export interface ButtonProps extends ParentProps {
+  onClick: (e) => void;
+  type: "primary" | "success" | "warn" | "error";
+  isBg: boolean;
+}
+const Button = (props: ButtonProps) => {
+  const { children, onClick, type = "primary", isBg = false } = props;
   return (
-    <div class={`${preName}-btn__${type}`}>
-      <button onClick={onClick}>{children}</button>
+    <div class={`${preName}-btn__${type}`} onClick={onClick}>
+      <div class={`${isBg ? `${preName}-btn__${type}-bg` : ""}`}>
+        <button>{children}</button>
+      </div>
     </div>
   );
 };
